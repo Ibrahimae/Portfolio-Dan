2d3d6532f3d419e9a192718846c4aea1ed791ad1
Ibrahimae <ibrahimaembalo93@gmail.com>
2024-12-19 06:38:57 +0200

    Description des changements

--- src/components/Introduction/index.jsx
+++ src/components/Introduction/index.jsx
@@ -5,12 +5,11 @@ function Introduction() {
         <div className="introduction">
             <h1>Bonjour ! je m’appelle</h1>
             <h2>Ibrahima Embalo.</h2>
-            <h3>Je suis développeur Frontend/Backend !</h3>
+            <h3>Je suis data analyst junior !</h3>
             <p>
-                Avec un diplome de developpeur web de OpenClassroom niveau 5 (equivalent bac+2) je suis spécialisé dans le frontend mais j'ai aussi des compétences en backend. En ce
-                moment, je suis
-                <span> à la recherche d'un job </span> en tant que
-                <span> développeur frontend/backend junior !</span>
+                Avec un diplome de data analyst d'OpenClassroom niveau 6 (equivalent bac+4) Je suis spécialisé dans l'analyse de données et la visualisation, mais j'ai également des compétences solides en développement backend et frontend. En ce moment, je suis 
+<span>à la recherche d'un emploi</span> en tant que 
+<span>Data Analyst junior</span>, où je pourrais mettre à profit mes compétences techniques pour explorer, analyser et valoriser les données, tout en créant des solutions automatisées et innovantes.
             </p>
             <a href="https://github.com/ibrahimae">
                 <button>Voici mon GitHub!</button>
